24521a9130244720f39755e64f15de9eb1186fc9
JosephVC <josephvcardenas@gmail.com>
2020-03-28 00:30:49 -0700

    made username and password

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData=[
     {name:'Mark'},
@@ -18,6 +21,27 @@ app.get('/users/:id', function(req, res){
         message: 'got one user, good shit',
         users: req.params.id
     })
+})
+
+app.post('/login', function(req, res) {
+    const username=req.body.username;
+    const password = req.body.password;
+
+    const mockUsername="assMan";
+    const mockPassword ="superSecret";
+
+    if(username==mockUsername && password==mockPassword) {
+        res.json({
+            success: true,
+            message:'passage and username match',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.join ({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
 })
 
 app.listen(8000, function() {
